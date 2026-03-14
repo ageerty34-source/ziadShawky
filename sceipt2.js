b47f55242c394ef5df4975dafeaca27cb60c46d5
ageerty34-source <ageerty34@gmail.com>
2026-03-14 15:44:38 +0200

    Add files via upload

---
+++ sceipt2.js
@@ -0,0 +1,16 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80
+    },
+    "size": {
+      "value": 5
+    },
+    "move": {
+      "speed": 2
+    },
+    "line_linked": {
+      "enable": true
+    }
+  }
+});
